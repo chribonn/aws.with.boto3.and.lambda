bd66b43e5b5b6e210589cd329e764c270bde2687
Alan C Bonnici <chribonn@gmail.com>
2022-06-29 15:07:19 +0200

    Session 11/06

--- 6.Usage of Meta object/1.Document-list-all-regions-for-ec2.py
+++ 6.Usage of Meta object/1.Document-list-all-regions-for-ec2.py
@@ -1,6 +1,6 @@
 import boto3
 aws_mag_con=boto3.session.Session(profile_name="root")
-ec2_con_re=aws_mag_con.resource(service_name="ec2")
+ec2_con_re=aws_mag_con.resource(service_name="ec2",region_name="us-east-1")
 
 for each_item in ec2_con_re.meta.client.describe_regions()['Regions']:
 	print(each_item['RegionName'])
